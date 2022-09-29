37fae699ee7564cea793284d86b2acce22a817d2
Robert Duddy <rduddy13@gmail.com>
2022-09-29 02:53:25 -0400

    react native navigation part 1

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,8 @@
+import RenderCampsite from "../features/campsites/RenderCampsite";
+
+const CampsiteInfoScreen = ({ route }) => {
+  const { campsite } = route.params;
+  return <RenderCampsite campsite={campsite} />;
+};
+
+export default CampsiteInfoScreen;
--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,10 +1,14 @@
-import { FlatList, Text } from "react-native";
+import { useState } from 'react';
+import { FlatList } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
+import { CAMPSITES } from '../shared/campsites';
 
-const DirectoryScreen = (props) => {
+const DirectoryScreen = ({ navigation }) => {
+  const [campsites, setCampsites] = useState(CAMPSITES);
+
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
-      <ListItem onPress={() => props.onPress(campsite.id)}>
+      <ListItem onPress={() => navigation.navigate('CampsiteInfo', { campsite })}>
         <Avatar source={campsite.image} rounded />
         <ListItem.Content>
           <ListItem.Title>{campsite.name}</ListItem.Title>
@@ -15,7 +19,7 @@ const DirectoryScreen = (props) => {
   };
   return (
     <FlatList
-      data={props.campsites}
+      data={campsites}
       renderItem={renderDirectoryItem}
       keyExtractor={(item) => item.id.toString()}
     />
