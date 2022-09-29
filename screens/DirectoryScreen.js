af24e7a60aeaad22cbcc5c72adf30968dd4b7c5f
Robert Duddy <rduddy13@gmail.com>
2022-09-29 00:42:27 -0400

    react native elements fine tuning

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,10 +1,10 @@
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 
 const DirectoryScreen = (props) => {
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
-      <ListItem>
+      <ListItem onPress={() => props.onPress(campsite.id)}>
         <Avatar source={campsite.image} rounded />
         <ListItem.Content>
           <ListItem.Title>{campsite.name}</ListItem.Title>
