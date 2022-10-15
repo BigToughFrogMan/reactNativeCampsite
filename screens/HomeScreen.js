4e6652c1cd8a0991dfa7b95d5c0de9ae56a7afb6
Robert Duddy <rduddy13@gmail.com>
2022-10-15 00:04:01 -0400

    homescreen animation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { ScrollView, Text, View } from "react-native";
+import { Text, View, Animated } from "react-native";
 import { Card } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "../components/LoadingComponent";
+import { useEffect, useRef } from 'react';
 
 const FeaturedItem = (props) => {
   const { item } = props;
@@ -42,16 +43,26 @@ const FeaturedItem = (props) => {
 };
 
 const HomeScreen = () => {
-  const campsites = useSelector((state) => (state.campsites));
-  const promotions = useSelector((state) => (state.promotions));
-  const partners = useSelector((state) => (state.partners));
+  const campsites = useSelector((state) => state.campsites);
+  const promotions = useSelector((state) => state.promotions);
+  const partners = useSelector((state) => state.partners);
+  const scaleValue = useRef(new Animated.Value(0)).current;
+  const scaleAnimation = Animated.timing(scaleValue, {
+    toValue: 1,
+    duration: 1500,
+    useNativeDriver: true
+  });
 
-  const featCampsite = campsites.campsitesArray.find((item) => (item.featured));
-  const featPromotion = promotions.promotionsArray.find((item) => (item.featured));
-  const featPartner = partners.partnersArray.find((item) => (item.featured));
+  const featCampsite = campsites.campsitesArray.find((item) => item.featured);
+  const featPromotion = promotions.promotionsArray.find((item) => item.featured);
+  const featPartner = partners.partnersArray.find((item) => item.featured);
+
+  useEffect(() => {
+    scaleAnimation.start();
+  }, []);
 
   return (
-    <ScrollView>
+    <Animated.ScrollView style={{ transform: [{ scale: scaleValue}]}}>
       <FeaturedItem
         item={featCampsite}
         isLoading={campsites.isLoading}
@@ -67,7 +78,7 @@ const HomeScreen = () => {
         isLoading={partners.isLoading}
         errMess={partners.errMess}
       />
-    </ScrollView>
+    </Animated.ScrollView>
   );
 };
 
