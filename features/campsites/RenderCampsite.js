63fde4c272441ffd58b168441ebaca57bd1205ab
Robert Duddy <rduddy13@gmail.com>
2022-10-15 03:16:07 -0400

    added rubberband animation to campsite favoriting gesture

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -2,14 +2,23 @@ import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
 import { Icon, Card } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from 'react-native-animatable';
+import { useRef } from 'react';
 
 const RenderCampsite = (props) => {
   const { campsite } = props;
+
+  const view = useRef();
 
   const isLeftSwipe = ({ dx }) => dx < -200;
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then((endState) => console.log(endState.finished ? 'finished' : 'canceled')
+        );
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log('pan responder end', gestureState);
       if (isLeftSwipe(gestureState)) {
@@ -41,6 +50,7 @@ const RenderCampsite = (props) => {
                     animation='fadeInDownBig'
                     duration={2000}
                     delay={1000}
+                    ref={view}
                     {...panResponder.panHandlers}
       >
         <Card containerStyle={styles.cardContainer}>
