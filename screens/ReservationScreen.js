8c97eaf2ce4f31e30e1f8679a2f820c5056c878a
Robert Duddy <rduddy13@gmail.com>
2022-10-21 23:54:19 -0400

    expo notification for reserved campsite

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -13,6 +13,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
   const [campers, setCampers] = useState(1);
@@ -40,12 +41,21 @@ const ReservationScreen = () => {
       [
         {
           text: 'Cancel',
-          onPress: () => console.log('Cancel Reservation Pressed'),
+          onPress: () => {
+            console.log('Cancel Reservation Pressed');
+            resetForm();
+          },
           style: 'cancel'
         },
         {
           text: 'OK',
-          onPress: () => console.log('Confirmed Reservation Search')
+          onPress: () => {
+            console.log('Confirmed Reservation Search');
+            presentLocalNotification(
+              date.toLocaleDateString('en-US')
+            );
+            resetForm();
+          }
         }
       ],
       { cancelable: false },
@@ -60,12 +70,40 @@ const ReservationScreen = () => {
     setShowCalendar(false);
   };
 
+  const presentLocalNotification = async (reservationDate) => {
+    const sendNotification = () => {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: true
+        })
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${reservationDate} requested`
+        },
+        trigger: null
+      });
+    };
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
+  };
+
   return (
-    <Animatable.View
-      animation='zoomIn'
-      duration={2000}
-      delay={1000}>
-      <ScrollView>
+    <ScrollView>
+      <Animatable.View
+        animation='zoomIn'
+        duration={2000}
+        delay={1000}>
         <View style={styles.formRow}>
           <Text style={styles.formLabel}>Number of Campers:</Text>
           <Picker
@@ -145,8 +183,8 @@ const ReservationScreen = () => {
                     />
                 </View>
               </Modal> */}
-      </ScrollView>
-    </Animatable.View>
+      </Animatable.View>
+    </ScrollView>
   );
 };
 
