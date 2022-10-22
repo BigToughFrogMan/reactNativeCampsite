85b39668a1470957f9ed2c47ec672b4978f109bf
Robert Duddy <rduddy13@gmail.com>
2022-10-22 00:38:19 -0400

    added social sharing from react native for campsites

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
+import { StyleSheet, Text, View, PanResponder, Alert, Share } from "react-native";
 import { Icon, Card } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from 'react-native-animatable';
@@ -49,6 +49,19 @@ const RenderCampsite = (props) => {
     }
   });
 
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+      {
+        title,
+        message: `${title}: ${message} ${url}`,
+        url
+      },
+      {
+        dialogTitle: 'Share ' + title
+      }
+    );
+  };
+
   if (campsite) {
     return (
       <Animatable.View
@@ -90,6 +103,20 @@ const RenderCampsite = (props) => {
               raised
               reverse
               onPress={() => props.onShowModal()}
+            />
+            < Icon
+              name='share'
+              type='font-awesome'
+              color='#5637DD'
+              raised
+              reverse
+              onPress={() => 
+                shareCampsite(
+                  campsite.name,
+                  campsite.description,
+                  baseUrl + campsite.image
+                )
+              }
             />
           </View>
         </Card>
